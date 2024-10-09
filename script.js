d28c28bf7ad604bfdad6698449d863a01402ff1d
Xylen <88765049+debxylen@users.noreply.github.com>
2024-10-10 01:34:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function _(element){
+   return document.querySelector(element);
+}
+
+
 function handleHashChange() {
     const hash = window.location.hash;
 
@@ -32,3 +37,49 @@ fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${ch
         document.getElementById('random-video').innerHTML = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" allowfullscreen></iframe>`;
     })
     .catch(error => console.error('Error fetching videos:', error));
+
+
+
+document.getElementById('about-me-button').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default anchor click behavior
+
+    const aboutSection = document.getElementById('about-section');
+    const asideSection1 = document.querySelector('.sidebar-right');
+    const asideSection2 = document.querySelector('.sidebar-left');
+    const updatesSection = document.querySelector('.updates');
+    const postsSection = document.querySelector('#posts');
+    const ph = postsSection.offsetHeight;
+    if (!aboutSection || !updatesSection || !postsSection) {
+        console.error("One or more sections not found!");
+        return;
+    }
+
+    // Find the posts link
+    const postsLink = document.querySelector('.posts-link');
+
+
+    // Add the "falldown" class to main and aside sections
+    updatesSection.classList.add('falldown');
+    asideSection1.classList.add('falldown');
+    asideSection2.classList.add('falldown');
+
+    // Listen for the animation end to hide the posts section
+    updatesSection.addEventListener('animationend', function() {
+
+        postsSection.style.height = `${ph}px`; //freeze height
+        updatesSection.style.display = 'none';
+        aboutSection.classList.remove('hidden');
+        aboutSection.classList.add('fade-in');
+
+        const aboutHeight = aboutSection.offsetHeight; // Get the height of the about section
+        postsSection.style.transition = 'height 0.5s ease'; // Add transition for height
+        postsSection.style.height = `${aboutHeight}px`; // Set the new height
+
+    }, { once: true });
+});
+
+
+
+
+
+
